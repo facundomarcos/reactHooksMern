274f6d192859456128182810b822a60c8027fc28
facundomarcos <facundomarcos@live.com.ar>
2023-03-29 09:48:42 -0300

    46

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -5,6 +5,9 @@ import PropTypes from 'prop-types';
 
  // props
  export const CounterApp = ({value}) =>{
+
+  console.log('render');
+  
     const [ counter, setCounter ] = useState( value );
 
    const handleAdd = () => 
@@ -13,13 +16,18 @@ import PropTypes from 'prop-types';
     //setCounter((c) => c + 1);
     }
 
+    const handleSubs = () =>  setCounter (counter - 1);
+    
+    const handleReset = () =>setCounter (value);
+
     return(
          <>
        <h1>Counter App</h1>
         <p> { counter } </p>
 
         <button onClick={handleAdd}> +1 </button>
-   
+        <button onClick={handleSubs}> -1 </button>
+        <button onClick={handleReset}> Reset </button>
         </>
        
     ) ;
