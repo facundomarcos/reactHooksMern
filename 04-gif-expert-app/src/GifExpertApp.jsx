4cba58990d870dd4d8e57b6c36c853392605d8ed
facundomarcos <facundomarcos@live.com.ar>
2023-04-03 11:39:06 -0300

    74

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -5,7 +5,15 @@
 export const GifExpertApp = () => {
   return (
     <>
+    {/* titulo */}
       <h1>GifExpertApp</h1>
+
+
+      {/* input */}
+
+
+      {/* listado */}
+         {/* gif item */}
     </>
   )
 }
