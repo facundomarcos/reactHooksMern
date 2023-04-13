b779be3548980194e82395eb5ef9fbfe86ea4b3a
facundomarcos <facundomarcos@live.com.ar>
2023-04-13 10:17:01 -0300

    82

---
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -0,0 +1,19 @@
+export const getGifs = async(categorie) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=vnZKO1CN6Q72ZkuJ33VVRWM44YO8NS7U&q=${categorie}&limit=20`;
+
+    const resp = await fetch(url);
+
+    const {data} = await resp.json();
+
+    //console.log(data);
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }))
+    
+    console.log(gifs);
+
+}
