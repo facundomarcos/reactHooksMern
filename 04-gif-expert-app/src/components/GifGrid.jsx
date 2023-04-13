5b888d6f4e33c0944dd9b2159a06f6a163efc9a1
facundomarcos <facundomarcos@live.com.ar>
2023-04-13 10:14:55 -0300

    82

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,21 +1,37 @@
 
+const getGifs = async(categorie) => {
 
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=vnZKO1CN6Q72ZkuJ33VVRWM44YO8NS7U&q=${categorie}&limit=20`;
+
+    const resp = await fetch(url);
+
+    const {data} = await resp.json();
+
+    //console.log(data);
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }))
+    
+    console.log(gifs);
+
+}
 
 
 export const GifGrid = ({categorie}) => {
 
-    const gifs = [1,2,3,4]
+   
+
+    getGifs(categorie);
 
   return (
-    <div>
+    <>
       <h3>{categorie}</h3>
-      {
-        gifs.map( gif => (
-            <p>{gif}</p>
-        ))
-      }
+     
       
-    </div>
+    </>
   )
 }
 
